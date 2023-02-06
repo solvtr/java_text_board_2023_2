71ff0c6348a785a1995a19a422ecde21b2767ce6
solvtr <solvtr@naver.com>
2023-02-06 20:52:39 +0900

    시작 알림 메시지 출출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -2,6 +2,9 @@ package com.sbs.exam.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("명령)");
+    System.out.println("== 프로그램 종료 ==");
   }
 }
